4b1b3846aa98e88c0a5d537815e8f48b5ee8cc0c
Chaitanya Singh <chaitanyasingh@Chaitanyas-MacBook-Pro.local>
2026-03-14 01:19:35 +0530

    .

--- Polymorphism/PracticeQ1.ts
+++ Polymorphism/PracticeQ1.ts
@@ -1,2 +1,12 @@
 // Compile-Time (Method Overloading)
 
+class Printer{
+    a : string
+    constructor(a:string){
+        this.a=a
+        
+        console.log(a)
+    }
+}
+
+console.log(new Printer("We ran our first code"))
